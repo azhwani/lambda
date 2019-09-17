4b4c5afeb85927e5ec922dcc443ee68d7daee088
Azhwani <azh.abderrahim@gmail.com>
2019-09-17 16:31:34 +0100

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,10 +2,12 @@ interface Hello{
     public String say();  
 }  
 public class LambdaExample3{  
-public static void main(String[] args) {  
-    Hello h=()->{  
-        return "Have a nice day!";  
-    };  
-    System.out.println(h.say());  
-}  
+    public static void main(String[] args) {  
+        // Interface implementation using lambda expression
+        Hello h=()->{  
+            return "Have a nice day!";  
+        }; 
+        
+        System.out.println(h.say());  
+    }  
 }  
